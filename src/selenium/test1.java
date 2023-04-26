396b0c25e69f0c4e799a5fa4b585a71fd2f97515
sagarchincholkar <120019025+sagarchincholkar@users.noreply.github.com>
2023-04-26 15:41:22 +0530

    Sagar: my first project completed

---
+++ src/selenium/test1.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32 (2).chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+	}
+
+}
